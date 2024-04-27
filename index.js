6f5da01bafac788fd335b8ac33ed273ede82478a
haqueashraful <123ashrafulhaque@gmail.com>
2024-04-27 09:07:52 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,44 @@
+require("dotenv").config();
+const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const cors = require('cors');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+
+const uri = process.env.DB_URI
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    const db = client.db("artCraftDB");
+    const artCraftCollection = db.collection("artCraftCollection");
+
+
+
+    app.get('/', (req, res) => {
+      res.send('Hello World!')
+    })
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
+run().catch(console.dir);
