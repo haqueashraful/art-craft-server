82dbe39e0bb865e778d5fa803cb4f49caa5249cd
haqueashraful <123ashrafulhaque@gmail.com>
2024-04-27 09:11:24 +0600

    delete req added

--- index.js
+++ index.js
@@ -59,7 +59,12 @@ async function run() {
     });
 
 
-
+    app.delete("/allArtCraft/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const result = await artCraftCollection.deleteOne(filter);
+      res.json(result);
+    });
 
 
     app.get('/', (req, res) => {
